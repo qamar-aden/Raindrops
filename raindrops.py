503b7eee34274e24d2989205d6d0bf4c3b709cda
Qamar Admin <qamaradmin@DAS00802.local>
2020-12-11 15:39:19 +0000

    Validate input function

--- raindrops.py
+++ raindrops.py
@@ -1,13 +1,17 @@
-#Input in a while loop to check for valid inputs
-while True:
-    try:
-# Input from the user
-        userInput = int(input("Pick a number: "))   
-    except ValueError:
-        print("Sorry, please use an integer")
-        continue
-    else:
-        break
+def check_input(value):
+    # Input in a while loop to check for valid inputs
+    while True:
+        try:
+    # Input from the user
+            userInput = int(input(value))   
+        except ValueError:
+            print("Sorry, please use an integer")
+            continue
+        else:
+            break
+    return userInput
+
+userInput = check_input("Pick a number: ")
 
 # Check Values
 if userInput % 3 == 0:
