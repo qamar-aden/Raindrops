b396ceecdbef6ffda72f54ed5b7ffa60b6eb61a8
Qamar Admin <qamaradmin@DAS00802.local>
2020-12-14 19:30:31 +0000

    Separated manual test from function. Added unittests to test the function and updated README.md

--- raindrops.py
+++ raindrops.py
@@ -1,26 +1,14 @@
-def check_input(value):
-    # Input in a while loop to check for valid inputs
-    while True:
-        try:
-    # Input from the user
-            userInput = int(input(value))   
-        except ValueError:
-            print("Sorry, please use an integer")
-            continue
-        else:
-            break
-    return userInput
-
-# Check Values Function
-def raindrop():
-    if userInput % 3 == 0:
-        print("Pling")
-    elif userInput % 5 == 0:
-        print("Plang")
-    elif userInput % 7 == 0:
-        print("Plong")
-    else:
-        print(userInput)
-
-userInput = check_input("Pick a number: ")
-raindrop()
+# Raindrops Function
+def get_raindrops(x):
+    # Put drops in array
+    drops = []
+    # Append array with values from 3,5,7 factors
+    if (x % 3 == 0):
+        drops.append("Pling")
+    if (x % 5 == 0):
+        drops.append("Plang")
+    if (x % 7 == 0):
+        drops.append("Plong")
+    if not drops:
+        return str(x)
+    return ''.join(drops)
